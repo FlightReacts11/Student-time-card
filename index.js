7e74dd7573231cd1174a2dab2ddea4bf1fed1d34
Gabriel <GCOverton@outlook.com>
2025-09-30 11:16:37 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('hello')
+document.addEventListener("DOMContentLoaded", function() {
+    // 8 categories // 
+    const NUM_OF_ROWS = 8;
+    const ROW_NAMES = [
+        "Safety Training",
+        "Health & First Aid",
+        "Admin Procedures",
+        "Child Nutrition & Hygiene",
+        "Child Development / Observation",
+        "Child Activity Supervision",
+        "Consultation Techniques"
+    ];
+
+    // Distribute Hours and Print Button //
+    const distributeBtn = document.getElementById("distribute-button");
+    const printBtn = document.getElementById("print-button");
+
+    // Month and Day Selectors //
+    const monthSelect = document.getElementById("month-select");
+    const daySelect = document.getElementById("day-select");
+
+    // Table Elements //
+    const tableContainer = document.getElementById("table-container");
+    const mainTable = document.getElementById("main-table");
+    const tableHead = document.getElementById("table-head");
+    const tableBody = document.getElementById("table-body");
+
+
+    // Event Listeners //
+    
+    distributeBtn.addEventListener("click", timeDistribution);
+    tableBody.addEventListener("input", generateTable)
+    printBtn.addEventListener("click", () => window.print());
+
+
+});
